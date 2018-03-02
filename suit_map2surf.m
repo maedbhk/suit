b99225b9ce84bd0fd475d6816c334db894514eba
Maedbh King <mking86@uwo.ca>
2018-03-02 15:17:29 -0800

    Add files via upload
    Includes structures as input - removes the need to create a .nii vol file to give as input.

--- suit_map2surf.m
+++ suit_map2surf.m
@@ -4,13 +4,19 @@ function Data=suit_map2surf(input,varargin)
 % representation 
 % 
 % INPUT: 
-%    Files:     Nx1 Array of filenames that should be mapped 
+% Files:        Nx1 Array of filenames that should be mapped 
 %               OR 
+%               Structure with fields: 
+%               V{N}.dim - voxel sizes (x,y,z)
+%               V{N}.mat - affine transform matrix
+%               V{N}.dat - vol data (x,y,z)
+%               OR
 %               Nx3 Matrix of foci with each row representing [x,y,z] of
 %               foci in atlas space
-% OUTPUT:       xxx x N Matrix of vertex values, if input were files 
+% OUTPUT:       xxx x N: Matrix of vertex values (xxx) by N,
+%               if input were files or structures
 %               OR 
-%               N x 2 Matrix of coordinates, if input were foci
+%               N x 2: Matrix of coordinates, if input were foci
 % VARARGIN: 
 %    'space':   SUIT/FSL/SPM: which normalisation routine was used for the
 %               images, determine the atlas surface used 
@@ -24,10 +30,14 @@ function Data=suit_map2surf(input,varargin)
 %               'nanmean'     Is default and used for activation data 
 %               'mode'        Should be used discrete labels are sampled and the most frequent label is assigned 
 %               'minORmax'    Maps the minumum or maximum values, glass brain projection for statistical maps
-%       
-% (c) j.diedrichsen@ucl.ac.uk, 2015
-% -----------------------------------------------------------------
+%    'flat_dir':  Flat file directory
 
+
+% (c) j.diedrichsen@ucl.ac.uk, 2015
+% Edited by Maedbh King (28/02/2017) to include structures as input.
+% Advantage of structure is that it removes the need to create a .nii volume
+% file to give as input to this function
+% -----------------------------------------------------------------
 % -----------------------------------------------------------------
 %   Set Defaults and deal with user options  
 % -----------------------------------------------------------------
@@ -44,14 +54,22 @@ flat_dir = [];
 depths   = [0 0.2 0.4 0.6 0.8 1];
 
 % For volume data
-ignore_zeros=1;       % In the volume-data, should zero be set to NaN? Normally yes for beta weights or t-values
+ignore_zeros=1;         % In the volume-data, should zero be set to NaN? Normally yes for beta weights or t-values
 stats=@nanmean;         % How are different sampling depth integrated? 
 
 % For foci data 
 depth_tolerance = 3;   % How many mm above or below the surface is it allowed to lie? 
 
-vararginoptions(varargin,{'pial','white','space','stats','ignore_zeros'});
+vararginoptions(varargin,{'pial','white','space','stats','ignore_zeros','flat_dir','depths'});
 numDepths=length(depths);
+
+if ~strmatch('pial', varargin) 
+    pial = sprintf('PIAL_%s.coord.gii',space);
+end
+
+if ~strmatch('white', varargin)
+    white = sprintf('WHITE_%s.coord.gii',space); 
+end; 
 
 SCCSid   = '3.1';
 SPMid    = spm('FnBanner',mfilename,SCCSid);
@@ -75,23 +93,21 @@ end;
 % -----------------------------------------------------------------
 %   Check the atlas space and load the appropriate surface files 
 % -----------------------------------------------------------------
-if (~isempty(space))
-    pial=sprintf('PIAL_%s.coord.gii',space); 
-    white=sprintf('WHITE_%s.coord.gii',space); 
-end; 
 try 
     C1=gifti(fullfile(flat_dir,pial));
     C2=gifti(fullfile(flat_dir,white));
+    
 catch 
     error('unknown space / pial or white file not found');
 end; 
+
 c1=C1.vertices;
 c2=C2.vertices; 
 
 % -----------------------------------------------------------------
-%   If input are image files: Map those. 
+%   If input is image file or structure: Map those. 
 % -----------------------------------------------------------------
-if (iscell(input) || ischar(input))
+if (iscell(input) || ischar(input)) || isstruct(input) % MK
 % If character array, make into cell
 if (ischar(input))
     for i=1:size(input,1);
@@ -101,21 +117,25 @@ else
     V=input; 
 end;
 A=[];
-firstgood=[]; 
-for i=1:size(V,2);
-    try
-        A{i}=spm_vol(V{i});
-        if (isempty(firstgood))
-            firstgood=i;
+firstgood=[];
+if iscell(V), % check for cell MK
+    for i=1:size(V,2);
+        try
+            A{i}=spm_vol(V{i});
+            if (isempty(firstgood))
+                firstgood=i;
+            end;
+        catch
+            warning(sprintf('%s could not be opened',V{i}));
+            A{i}=[];
         end;
-    catch
-        warning(sprintf('%s could not be opened',V{i}));
-        A{i}=[]; 
     end;
-end;
+else % if struct
+    A=V; 
+end
 V=A;
 
-if (isempty(firstgood))
+if (isempty(firstgood)) && ~isstruct(input), % MK
     error('none of the images could be opened');
 end; 
 
@@ -127,8 +147,16 @@ end;
 %   Loop over the nodes depth and get the linear voxel indices 
 % -----------------------------------------------------------------
 % For all sampling depth - get the 
-dim = V{firstgood}.dim;
-mat = V{firstgood}.mat;
+
+if ~isstruct(V)
+    dim = V{firstgood}.dim;
+    mat = V{firstgood}.mat;
+else
+    dim = V.dim;
+    mat = V.mat;
+    V = {V};     %Converting to cell for rest of script
+end
+
 for d=1:numDepths % Number of sample points between white and pial surface
     c=(1-depths(d))*c1+depths(d)*c2;
     [i,j,k]=spmj_affine_transform(c(:,1),c(:,2),c(:,3),inv(mat));
@@ -139,6 +167,7 @@ for d=1:numDepths % Number of sample points between white and pial surface
     outside = i<1 | i>dim(1) | j<1 | j>dim(2) | k<1 | k>dim(3); 
     indices(outside,d)=NaN; 
 end;
+
 i=find(~isnan(indices));
 data=zeros(size(indices))*NaN;
 
@@ -147,7 +176,11 @@ data=zeros(size(indices))*NaN;
 % -----------------------------------------------------------------
 for v=1:length(V);
     if (~isempty(V{v}))
-        X=spm_read_vols(V{v});
+        if isfield(V{v},'fname'),% MK 
+            X=spm_read_vols(V{v}); 
+        else
+            X=V{v}.dat; 
+        end
         if (ignore_zeros)
             X(X==0)=NaN;
         end;
